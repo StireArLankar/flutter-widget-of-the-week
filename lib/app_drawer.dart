06d24f7eb41ec8746fda34ed51c04dcb556ae6aa
Stire <stire.ru@gmail.com>
2020-01-15 23:57:30 +0300

    Refactored routes

--- lib/app_drawer.dart
+++ lib/app_drawer.dart
@@ -1,23 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_widget_of_the_week/1_safe_area/safe_area.dart';
-import 'package:flutter_widget_of_the_week/2_expanded/expanded.dart';
-import 'package:flutter_widget_of_the_week/3_wrap/wrap.dart';
-import 'package:flutter_widget_of_the_week/4_animated_container/animated_container.dart';
-import 'package:flutter_widget_of_the_week/5_opacity/opacity.dart';
-import 'package:flutter_widget_of_the_week/6_future_builder/future_builder.dart';
+
+import './router.dart';
 
 class AppDrawer extends StatelessWidget {
-  List<Widget> buildLink(BuildContext context, String title, String route) {
-    return [
-      ListTile(
-        leading: const Icon(Icons.panorama_fish_eye),
-        title: Text(title),
-        onTap: () {
-          Navigator.of(context).pushReplacementNamed(route);
-        },
-      ),
-      Divider(),
-    ];
+  Widget buildLink(BuildContext context, String title, String route) {
+    return ListTile(
+      leading: const Icon(Icons.panorama_fish_eye),
+      title: Text(title),
+      onTap: () {
+        Navigator.of(context).pushReplacementNamed(route);
+      },
+    );
   }
 
   @override
@@ -35,17 +28,9 @@ class AppDrawer extends StatelessWidget {
             Expanded(
               child: SingleChildScrollView(
                 child: Column(
-                  children: <Widget>[
-                    ...buildLink(
-                        context, 'Safe Area', SafeAreaScreen.routeName),
-                    ...buildLink(context, 'Expanded', ExpandedScreen.routeName),
-                    ...buildLink(context, 'Wrap', WrapScreen.routeName),
-                    ...buildLink(context, 'Animated Container',
-                        AnimatedContainerScreen.routeName),
-                    ...buildLink(context, 'Opacity', OpacityScreen.routeName),
-                    ...buildLink(context, 'Future Builder',
-                        FutureBuilderScreen.routeName),
-                  ],
+                  children: routes.map((route) {
+                    return buildLink(context, route.title, route.routeName);
+                  }).toList(),
                 ),
               ),
             ),
--- lib/router.dart
+++ lib/router.dart
@@ -8,23 +8,81 @@ import 'package:flutter_widget_of_the_week/5_opacity/opacity.dart';
 import 'package:flutter_widget_of_the_week/6_future_builder/future_builder.dart';
 import 'package:flutter_widget_of_the_week/app_drawer.dart';
 
+class RouteItem {
+  final Widget Function(BuildContext) screenBuilder;
+  final String routeName;
+  final String title;
+
+  const RouteItem(
+    this.screenBuilder,
+    this.routeName,
+    this.title,
+  );
+}
+
+Widget fun(BuildContext ctx) {
+  return SafeAreaScreen();
+}
+
+final routes = [
+  RouteItem(
+    (ctx) => SafeAreaScreen(),
+    SafeAreaScreen.routeName,
+    'Safe Area',
+  ),
+  RouteItem(
+    (ctx) => ExpandedScreen(),
+    ExpandedScreen.routeName,
+    'Expanded',
+  ),
+  RouteItem(
+    (ctx) => WrapScreen(),
+    WrapScreen.routeName,
+    'Wrap',
+  ),
+  RouteItem(
+    (ctx) => AnimatedContainerScreen(),
+    AnimatedContainerScreen.routeName,
+    'Animated Container',
+  ),
+  RouteItem(
+    (ctx) => OpacityScreen(),
+    OpacityScreen.routeName,
+    'Opacity',
+  ),
+  RouteItem(
+    (ctx) => FutureBuilderScreen(),
+    FutureBuilderScreen.routeName,
+    'Future Builder',
+  ),
+];
+
 Route<dynamic> generateRoutes(RouteSettings settings) {
-  switch (settings.name) {
-    case SafeAreaScreen.routeName:
-      return MaterialPageRoute(builder: (_) => SafeAreaScreen());
-    case ExpandedScreen.routeName:
-      return MaterialPageRoute(builder: (_) => ExpandedScreen());
-    case WrapScreen.routeName:
-      return MaterialPageRoute(builder: (_) => WrapScreen());
-    case AnimatedContainerScreen.routeName:
-      return MaterialPageRoute(builder: (_) => AnimatedContainerScreen());
-    case OpacityScreen.routeName:
-      return MaterialPageRoute(builder: (_) => OpacityScreen());
-    case FutureBuilderScreen.routeName:
-      return MaterialPageRoute(builder: (_) => FutureBuilderScreen());
-    default:
-      return MaterialPageRoute(builder: (_) => DefaultScreen(settings.name));
+  final route = routes.firstWhere((route) {
+    return route.routeName == settings.name;
+  }, orElse: () => null);
+
+  if (route == null) {
+    return MaterialPageRoute(builder: (_) => DefaultScreen(settings.name));
   }
+
+  return MaterialPageRoute(builder: route.screenBuilder);
+  // switch (settings.name) {
+  //   case SafeAreaScreen.routeName:
+  //     return MaterialPageRoute(builder: (_) => SafeAreaScreen());
+  //   case ExpandedScreen.routeName:
+  //     return MaterialPageRoute(builder: (_) => ExpandedScreen());
+  //   case WrapScreen.routeName:
+  //     return MaterialPageRoute(builder: (_) => WrapScreen());
+  //   case AnimatedContainerScreen.routeName:
+  //     return MaterialPageRoute(builder: (_) => AnimatedContainerScreen());
+  //   case OpacityScreen.routeName:
+  //     return MaterialPageRoute(builder: (_) => OpacityScreen());
+  //   case FutureBuilderScreen.routeName:
+  //     return MaterialPageRoute(builder: (_) => FutureBuilderScreen());
+  //   default:
+  //     return MaterialPageRoute(builder: (_) => DefaultScreen(settings.name));
+  // }
 }
 
 class DefaultScreen extends StatelessWidget {
